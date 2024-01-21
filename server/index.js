ae98f2dee7b550c308ad3d82674dbbcfd53a8196
Alan Amalemba <alanlsamalemba@gmail.com>
2024-01-21 07:21:11 +0300

    Set up node server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log("Hello world");
+const express = require("express");
+const db = require("./models");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Routers
+
+//...
+
+db.sequelize.sync().then(() => {
+  app.listen(8000, () => {
+    console.log("--- Server running at port 8000 ---");
+  });
+});
